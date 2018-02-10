1ad7839dca1bdbb3c24cccfe0ea79557f9200f0e
luqinmao <1002438809@qq.com>
2018-02-10 18:02:00 +0800

    rxjava 操作符

--- app/src/main/java/com/lqm/mvpstudy/rxjava/TestRxJavaActivity.java
+++ app/src/main/java/com/lqm/mvpstudy/rxjava/TestRxJavaActivity.java
@@ -42,7 +42,42 @@ public class TestRxJavaActivity extends Activity {
                 }
             });
 
-
-
     }
+
+
+//    {
+//
+//        final List<Bitmap> imageCollectorView = new ArrayList<>();
+//        File[] folders = new File[0];
+//        Observable.from(folders)
+//            .flatMap(new Func1<File, Observable<File>>() {
+//                @Override
+//                public Observable<File> call(File file) {
+//                    return Observable.from(file.listFiles());
+//                }
+//            })
+//            .filter(new Func1<File, Boolean>() {
+//                @Override
+//                public Boolean call(File file) {
+//                    return file.getName().endsWith(".png");
+//                }
+//            })
+//            .map(new Func1<File, Bitmap>() {
+//                @Override
+//                public Bitmap call(File file) {
+//                    return getBitmapFromFile(file);
+//                }
+//            })
+//            .subscribeOn(Schedulers.io())
+//            .observeOn(AndroidSchedulers.mainThread())
+//            .subscribe(new Action1<Bitmap>() {
+//                @Override
+//                public void call(Bitmap bitmap) {
+//                    imageCollectorView.add(bitmap);
+//                }
+//            });
+//
+//
+//    }
+
 }
